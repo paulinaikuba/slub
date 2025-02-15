120842eec736ef0f3bc36af5f07dbf2cbec94972
Jakub Ziec <jakziec@gmail.com>
2025-02-15 21:49:39 +0100

    Fix Google Maps

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,25 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 
-export default defineConfig({
-    base: './',
-    root: 'src',
-    build: {
-        outDir: '../dist'
-    },
-    server: {
-        port: 3000,
-        open: true
-    },
-    publicDir: '../public'
-})
+export default defineConfig(({ mode }) => {
+        // Load env file based on `mode` in the current working directory.
+        // Set the third parameter to '' to load all env regardless of the
+        // `VITE_` prefix.
+        const env = loadEnv(mode, process.cwd(), '')
+        return {
+            // vite config
+            define: {
+                __APP_ENV__: JSON.stringify(env.APP_ENV),
+            },
+            base: './',
+            root: 'src',
+            build: {
+                outDir: '../dist'
+            },
+            server: {
+                port: 3000,
+                open: true
+            },
+            publicDir: '../public'
+        }
+    }
+)
